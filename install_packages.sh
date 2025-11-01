0b73a437c57cfd11e3bd9f55c4a49283f8ad0557
polina <polyastrikesagain@gmail.com>
2025-11-01 17:17:37 +0300

    Added script to install packages to be able to run pytest and do API calls

---
+++ install_packages.sh
@@ -0,0 +1,22 @@
+echo "hi! i'm gonna ask your password to install python and pip and then install all the libraries that are missing"
+  if command -v apt &> /dev/null;
+then sudo apt install python3 python3-pip
+elif command -v dnf &> /dev/null;
+then sudo dnf install python3 python3-pip
+elif command -v yum &> /dev/null;
+then sudo yum install python3 python3-pip
+elif command -v pacman &> /dev/null;
+then sudo pacman -S python python-pip
+else read -p "can't find your package manager. i'm no linus torvalds. sorry. press ENTER to exit" && exit
+fi
+
+if python3 -m pip &> /dev/null;
+then python3 -m pip install pytest requests typing
+elif python -m pip &> /dev/null;
+then python -m pip install pytest requests typing
+else read -p "some problem with pip installation. press ENTER to exit" && exit
+fi
+echo ''
+echo '^_^'
+echo ''
+read -p "everything's installed, now run run_tests.sh the same way you ran this script. press ENTER to exit and go on"
