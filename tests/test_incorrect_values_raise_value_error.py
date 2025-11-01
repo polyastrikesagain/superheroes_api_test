e9059468dfc3bbc50f5cabe5e1498a09ad11ab1c
polina <polyastrikesagain@gmail.com>
2025-11-01 16:49:56 +0300

    Added tests to see if function raises ValueError if wrong values are given

--- tests/test_incorrect_values_raise_value_error.py
+++ tests/test_incorrect_values_raise_value_error.py
@@ -2,6 +2,8 @@ import pytest
 from function import tallest_of_filtered
 from typing import Any
 
+# тут я постаралась покрыть все проблемы, которые могут вернуть ValueError, и попарно тестируем +/-, -/+, -/-
+
 @pytest.mark.parametrize("gender, is_employed", [
     ("m@l3", 4),
     ("femail", True),
@@ -14,5 +16,5 @@ from typing import Any
 ])
 def test_incorrect_values_raise_value_error(call_superheroes_api: list[dict[str, Any]], gender: str, is_employed: bool):
     superheroes_json = call_superheroes_api
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError):         # если возвращается ошибка ValueError, то все отлично, негативный тест!
         tallest_of_filtered(gender, is_employed, superheroes_json)
